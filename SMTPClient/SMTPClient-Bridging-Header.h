7a9f56b06ca1eb9df6e0b0c3f3bf168b361d7ff4
mzgk <mzgkworks@gmail.com>
2018-03-08 16:18:54 +0900

    add: MailCoreを使用するためのBridging-Header.hと設定を追加

---
+++ SMTPClient/SMTPClient-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SMTPClient-Bridging-Header.h
+//  SMTPClient
+//
+//  Created by Mizugaki on 2018/03/08.
+//  Copyright © 2018年 edu.com. All rights reserved.
+//
+
+#ifndef SMTPClient_Bridging_Header_h
+#define SMTPClient_Bridging_Header_h
+#import <MailCore/MailCore.h>
+
+#endif /* SMTPClient_Bridging_Header_h */
