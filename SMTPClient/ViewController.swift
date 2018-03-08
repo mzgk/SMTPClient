585cb23ab640ffc42908a8dc862676de6c1dbd36
mzgk <mzgkworks@gmail.com>
2018-03-08 13:58:27 +0900

    Initial Commit

---
+++ SMTPClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMTPClient
+//
+//  Created by Mizugaki on 2018/03/08.
+//  Copyright © 2018年 edu.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
